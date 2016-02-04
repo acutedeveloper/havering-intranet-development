6fb8c89956dee6d4a363984bf90794df84cb276e
Nigel M Peters <develop@acumendesign.co.uk>
2016-02-04 16:39:46 +0000

    Updated archive page template

--- wp-content/themes/havering-intranet/archive.php
+++ wp-content/themes/havering-intranet/archive.php
@@ -10,19 +10,46 @@ Template Name: Archives
 
 // Everything is being feed by the wp_nav_menu
 
-get_header(); ?>
+	get_header();
 
-		<?php
+  ?>
 
-      $current_category = get_queried_object();
+		<div class="three-quarters-grid">
+	  	<div class="left-column">
 
-      // printme($current_category->query_var);
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				?>
 
-    ?>
+				<hr/>
 
-    <div class="one-up-grid">
-    	<div id="content-navigation">
-					<h1>Here is the archive page</h1>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+					<div class="block <?php echo (has_post_thumbnail() ? 'block-thumb' : 'block-headline') ?>">
+		        <?php
+		              if ( has_post_thumbnail() )
+		              {
+		                echo '<div class="b-thumb">';
+		                the_post_thumbnail();
+		                echo '</div>';
+		              }
+		        ?>
+		        <div class="b-text">
+		          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		          <time datetime="<?php echo get_the_date('Y-d-jTh:i:s'); ?>">Posted on: <?php echo get_the_date(); ?></time>
+		          <?php the_excerpt(); ?>
+		          <a href="<?php the_permalink(); ?>">Read more <i class="fa fa-chevron-circle-right"></i></a>
+		        </div>
+		      </div>
+
+				<?php endwhile; else : _e( 'Sorry, no posts matched your criteria.', 'textdomain' ); endif; ?>
+				<?php paginate_links (); ?>
     	</div>
+
+			<div class="right-column">
+
+	      <?php dynamic_sidebar( 'news_sidebar' ); ?>
+
+	  	</div>
     </div>
 <?php get_footer(); ?>
