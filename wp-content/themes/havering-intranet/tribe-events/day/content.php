e74d2959d56c6b1bec1dcb54288ca4dc77a2758a
Nigel M Peters <develop@acumendesign.co.uk>
2016-02-16 17:05:43 +0000

    New css for tribe events templates

--- wp-content/themes/havering-intranet/tribe-events/day/content.php
+++ wp-content/themes/havering-intranet/tribe-events/day/content.php
@@ -14,9 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 } ?>
 
-asdfasdfsadfsdfsadfsadfsd
-
-<div id="tribe-events-content" class="tribe-events-list tribe-events-day">
+<div id="tribe-events-content" class="tribe-events-list tribe-events-day container">
 
 	<!-- List Title -->
 	<?php do_action( 'tribe_events_before_the_title' ); ?>
