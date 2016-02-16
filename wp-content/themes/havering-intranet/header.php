9ba212b0a8eccec8737dbe6da4a89588f545d73d
Nigel M Peters <develop@acumendesign.co.uk>
2016-02-16 14:54:34 +0000

    updated the carousel to WP_Query

--- wp-content/themes/havering-intranet/header.php
+++ wp-content/themes/havering-intranet/header.php
@@ -11,9 +11,12 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/library/js/bxslider/jquery.bxslider.css" media="all" />
 
+
+
 		<?php wp_head(); ?>
 	</head>
 	<body class="body">
+
     <div class="wrapper">
 
       <div class="header-search">
@@ -28,14 +31,25 @@
       	</form>
       </div>
 
-				<?php query_posts( 'p=1734&post_type=page' ); ?>
+				<?php
+
+					// To get carousel items page
+					$args = array (
+						'p'                      => '1734',
+						'post_type'              => array( 'page' ),
+					);
+
+					// The Query
+					$query = new WP_Query( $args );
+
+				?>
 
 	      <header id="header" class="cf" role="banner">
       	<div class="container">
       		<div class="logo">
       			<a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/library/images/logo-havering-intranet.png" alt="Havering Intranet" /></a>
       		</div>
-					<?php while (have_posts()) : the_post(); ?>
+					<?php while ($query->have_posts()) : $query->the_post(); ?>
       		<ul class="bxslider">
 					<?php
 						if( have_rows('carousel_images') ): while ( have_rows('carousel_images') ) : the_row();
@@ -44,7 +58,7 @@
       			<li><a href="<?php echo get_sub_field('carousel_link'); ?>"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['name']; ?>" /></a></li>
 					<?php endwhile; endif; ?>
       		</ul>
-				<?php endwhile; wp_reset_query(); ?>
+				<?php endwhile; wp_reset_query(); rewind_posts(); ?>
       	</div>
       </header>
 
