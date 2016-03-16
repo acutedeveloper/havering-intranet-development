7a0819ba8f679bac811dc88bbe9a6ff63fd20b89
Nigel M Peters <develop@acumendesign.co.uk>
2016-03-16 16:26:06 +0000

    Updated cpts for user capabilities

--- wp-content/plugins/intranet-plugins/includes/class-custom-posttypes.php
+++ wp-content/plugins/intranet-plugins/includes/class-custom-posttypes.php
@@ -10,60 +10,63 @@ $hi_cpts = array(
 	array(
 		'name' => 'Help with work',
 		'cpt_name' => 'hi_help_with_work',
+		'cpt_shortname' => 'hi_hww',
 		'cpt_slug' => 'help-with-work',
 		'cpt_taxonomy' => 'hi_help_with_work_tax'
 	),
 	array(
 		'name' => 'Support for you',
 		'cpt_name' => 'hi_support_for_you',
+		'cpt_shortname' => 'hi_sfy',
 		'cpt_slug' => 'support-for-you',
 		'cpt_taxonomy' => 'hi_support_for_you_tax'
 	),
-	// array(
-	// 	'name' => 'Human Resources 2 Char',
-	// 	'cpt_name' => 'hi_hr',
-	// 	'cpt_slug' => 'hr',
-	// 	'cpt_taxonomy' => 'hi_hr_tax'
-	// ),
 	array(
 		'name' => 'Human Resources',
 		'cpt_name' => 'hi_human_resources',
+		'cpt_shortname' => 'hi_humanre',
 		'cpt_slug' => 'human-resources',
 		'cpt_taxonomy' => 'hi_human_resources_tax'
 	),
 	array(
 		'name' => 'Self Service',
 		'cpt_name' => 'hi_self_service',
+		'cpt_shortname' => 'hi_selser',
 		'cpt_slug' => 'self-service',
 		'cpt_taxonomy' => 'hi_self_service_tax'
 	),
 	array(
 		'name' => 'ICT',
 		'cpt_name' => 'hi_ict',
+		'cpt_shortname' => 'hi_ict',
 		'cpt_slug' => 'ict',
 		'cpt_taxonomy' => 'hi_ict_tax'
 	),
 	array(
 		'name' => 'Committee Services',
 		'cpt_name' => 'hi_committee_service',
+		'cpt_shortname' => 'hi_comser',
 		'cpt_slug' => 'committee-services',
 		'cpt_taxonomy' => 'hi_committee_serv_tax'
 	),
 	array(
 		'name' => 'Health and Safety',
 		'cpt_name' => 'hi_health_safety',
+		'cpt_shortname' => 'hi_has',
 		'cpt_slug' => 'health-and-safety',
 		'cpt_taxonomy' => 'hi_health_safety_tax'
 	),
 	array(
 		'name' => 'Our Services',
 		'cpt_name' => 'hi_our_services',
+		'cpt_shortname' => 'hi_ourser',
 		'cpt_slug' => 'our-services',
 		'cpt_taxonomy' => 'hi_our_services_tax'
 	),
 	array(
 		'name' => 'About Havering',
 		'cpt_name' => 'hi_about_havering',
+		'cpt_shortname' => 'hi_abhav',
 		'cpt_slug' => 'about-havering',
 		'cpt_taxonomy' => 'hi_about_havering_tax'
 	),
@@ -79,8 +82,8 @@ class Build_intranet_cpts {
 
     function __construct($array) {
         $this->cpts_array = $array;
+				add_action( 'init', array(&$this, 'add_intranet_post_types'), 0);
 				add_action( 'init', array(&$this, 'add_intranet_taxonomies') );
-				add_action( 'init', array(&$this, 'add_intranet_post_types') );
 				add_action( 'init', array(&$this, 'register_custom_permalinks') );
 
 				// This is to setup the link to render the custom permalink structures
@@ -99,6 +102,19 @@ class Build_intranet_cpts {
 					'name_admin_bar'        => __( $hi_cpt['name'], 'text_domain' ),
 				);
 
+				$capabilities = array(
+					'publish_posts' => 'publish_'.$hi_cpt['cpt_name'],
+					'edit_published_posts' => 'edit_published_'.$hi_cpt['cpt_name'],
+					'edit_posts' => 'edit_'.$hi_cpt['cpt_name'],
+					'edit_others_posts' => 'edit_others_'.$hi_cpt['cpt_name'],
+					'delete_posts' => 'delete_'.$hi_cpt['cpt_name'],
+					'delete_others_posts' => 'delete_others_'.$hi_cpt['cpt_name'],
+					'read_private_posts' => 'read_private_'.$hi_cpt['cpt_name'],
+					'edit_post' => 'edit_'.$hi_cpt['cpt_name'],
+					'delete_post' => 'delete_'.$hi_cpt['cpt_name'],
+					'read_post' => 'read_'.$hi_cpt['cpt_name'],
+				);
+
 				$args = array(
 					'label'                 => __( $hi_cpt['name'], 'text_domain' ),
 					'description'           => __( $hi_cpt['name'], 'text_domain' ),
@@ -107,12 +123,14 @@ class Build_intranet_cpts {
 					'rewrite' 							=> array('slug' => $hi_cpt['cpt_slug']),
 					'hierarchical'          => true,
 					'public'                => true,
+					'show_ui'								=> true,
 					'menu_position'         => 5,
 					'menu_icon'             => 'dashicons-admin-page',
 					'can_export'            => true,
 					'has_archive'           => true,
 					'exclude_from_search'   => false,
 					'capability_type'       => 'page',
+					'capabilities'					=> $capabilities,
 
 				);
 
